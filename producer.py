ae582c775579e3d47bc7c113e87fe03c55473881
monstrillllo <evgeniy27112000@gmail.com>
2021-10-06 10:28:25 +0300

    fix

--- producer.py
+++ producer.py
@@ -5,10 +5,10 @@ from string import digits, ascii_letters
 from kafka import KafkaProducer
 
 
-def producer_send(value_type: str, value: str, partition: int):
+def producer_send(value_type: str, value: str):
     producer = KafkaProducer(bootstrap_servers=f'{os.environ["KAFKA_IP"]}', value_serializer=str.encode, key_serializer=str.encode)
-    producer.send(topic='course-topic', value=value, key=value_type, partition=partition)
-    print(f'[->]send key: {value_type}, value: {value}, partition: {partition}')
+    producer.send(topic='course-topic', value=value, key=value_type)
+    print(f'[->]send key: {value_type}, value: {value}')
 
 
 def main():
@@ -23,7 +23,7 @@ def main():
             value = ''
             for _ in range(len_):
                 value += random.choice(ascii_letters)
-        producer_send(choice, value, random.randrange(0, 2))
+        producer_send(choice, value)
         time.sleep(10)
 
 
